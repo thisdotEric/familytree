8e5ec143e53744ed68337bc4d5ab1345ebbb8344
thisdotEric <siguenzajohneric@gmail.com>
2022-07-12 21:38:29 +0800

    Updated App.tsx routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Families from './pages/Family';
+import Layout from './pages/Layout';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Families />} />
+        <Route path='/' element={<Layout />}>
+          <Route path='' element={<Families />} />
+        </Route>
       </Routes>
     </Router>
   );
