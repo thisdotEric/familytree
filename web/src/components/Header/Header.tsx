abd23f88b860900d3aa61163a7aa508cbd8dcf25
thisdotEric <siguenzajohneric@gmail.com>
2022-07-13 10:39:16 +0800

    Changed app name to a link to homepage

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 interface HeaderProps {
@@ -8,9 +9,11 @@ interface HeaderProps {
 const Header: FC<HeaderProps> = ({ banner }: HeaderProps) => {
   return (
     <div className='header-wrapper'>
-      <p id='app-name'>
-        family<span id='tree'>tree</span>
-      </p>
+      <Link to={'/'} id='home-link'>
+        <p id='app-name'>
+          family<span id='tree'>tree</span>
+        </p>
+      </Link>
       {banner && (
         <p id='inner-text'>
           of the <span id='family-name'>{banner}</span>'s
